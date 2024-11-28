7a962c512bcbf3f91099e6c425015b5d8167d4ad
zuamaya <a01784238@tec.mx>
2024-11-28 15:41:43 -0600

    new evidence two

---
+++ Ev2Algo/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void read_file_matrix(vector<vector<int>> &matrix, string file_name)
+{
+    ifstream file(file_name);
+}
+
+int main()
+{
+    return 0;
+}
